f75fb1cbee57bc22f3dd37f777dd329fcb5791f2
yinguobing <yinguobing@gmail.com>
2020-08-06 17:25:16 +0800

    acomplished: face alignment and cropping

--- generate_dataset.py
+++ generate_dataset.py
@@ -83,65 +83,43 @@ def process(dataset, index_start_from=0):
 
             # Safety check, invalid samples will be discarded.
             image = sample.read_image()
-            img_height, img_width, _ = image.shape
             marks = sample.marks
 
             # Security check passed, the image is ready for transformation. Here
             # the face area is our region of interest, and will be cropped.
-
             # First, move the face to the center.
-            face_center = mo.get_center(marks)[:2]
-            translation_mat = np.array([[1, 0, img_width / 2 - face_center[0]],
-                                        [0, 1, img_height / 2 - face_center[1]]])
-            translated_image = cv2.warpAffine(
-                image, translation_mat, (img_width, img_height))
+            image_translated, trans_vector = move_face_to_center(
+                image, marks, mo)
+            marks_translated = marks[:, :2] + trans_vector
 
             # Second, align the face. This happens in the 2D space.
-            key_marks = sample.get_key_marks()[:, :2]
-            vector_eye = (key_marks[3] - key_marks[0])
-            degrees = mo.get_angle(vector_eye, np.array([100, 0]))
-            rotation_mat = cv2.getRotationMatrix2D(
-                ((img_width-1)/2.0, (img_height-1)/2.0), -degrees, 1)
-            image_rotated = cv2.warpAffine(
-                translated_image, rotation_mat, (img_width, img_height))
+            image_rotated, degrees = rotate_to_vertical(
+                image_translated, sample, mo)
+            img_height, img_width, _ = image.shape
+            marks_rotated = mo.rotate(
+                marks_translated, degrees/180*np.pi, (img_width/2, img_height/2))
 
-            # Third, try to crop the face area out.
-            x_min, y_min, _ = np.amin(marks, 0)
-            x_max, y_max, _ = np.amax(marks, 0)
-            scale = 1.5
-            side_length = max((x_max - x_min, y_max - y_min)) * scale
-            start_x = int(img_width / 2 - side_length / 2)
-            start_y = int(img_height / 2 - side_length / 2)
-            end_x = int(img_width / 2 + side_length / 2)
-            end_y = int(img_height / 2 + side_length / 2)
-
-            # In case the new bbox is out of image bounding.
-            border_width = 0
-            border_x = min(start_x, start_y)
-            border_y = max(end_x - img_width, end_y - img_height)
-            if border_x < 0 or border_y > 0:
-                border_width = max(abs(border_x), abs(border_y))
-                start_x += border_width
-                start_y += border_width
-                end_x += border_width
-                end_y += border_width
-                image_with_border = cv2.copyMakeBorder(image, border_width,
-                                                       border_width,
-                                                       border_width,
-                                                       border_width,
-                                                       cv2.BORDER_CONSTANT,
-                                                       value=[0, 0, 0])
-                image_cropped = image_with_border[start_y:end_y,
-                                                  start_x:end_x]
-            else:
-                image_cropped = image_rotated[start_y:end_y, start_x:end_x]
+            # Third, try to crop the face area out. Pad the image if necessary.
+            image_cropped, padding, bbox = crop_face(image_rotated, marks)
+            mark_cropped = marks_rotated + \
+                padding - np.array([bbox[0], bbox[1]])
 
             # Last, resize the face area. I noticed Google is using 192px.
             image_resized = cv2.resize(image_cropped, (192, 192))
+            mark_resized = mark_cropped * (192 / image_cropped.shape[0])
 
-            cv2.imshow("Preview", image_resized)
-            if cv2.waitKey(30) == 27:
-                break
+            # Show all the image processed in debug mode.
+            if args.loglevel.upper() == "DEBUG":
+                esc_key = show_debug_images(image_translated, marks_translated,
+                                            image_rotated, marks_rotated,
+                                            image_resized, mark_resized,
+                                            padding, bbox)
+                if esc_key:
+                    break
+
+            # Now the cropped face and marks are available, do whatever you want.
+            # TODO: Generate face mesh.
+
     except Exception:
         logger.error(
             "Error {}. sample index: {}".format(traceback.format_exc(), current_sample_index))
@@ -149,6 +127,101 @@ def process(dataset, index_start_from=0):
         # Summary
         logger.info("Dataset done. Processed samples: {}, invalid samples: {}".format(
             current_sample_index+1, num_invalid_samples))
+
+
+def move_face_to_center(image, marks, mo):
+    img_height, img_width, _ = image.shape
+    face_center = mo.get_center(marks)[:2]
+    translation_mat = np.array([[1, 0, img_width / 2 - face_center[0]],
+                                [0, 1, img_height / 2 - face_center[1]]])
+    image_translated = cv2.warpAffine(
+        image, translation_mat, (img_width, img_height))
+
+    translation_vector = np.array(
+        [img_width / 2 - face_center[0],  img_height / 2 - face_center[1]])
+
+    return image_translated, translation_vector
+
+
+def rotate_to_vertical(image, sample, mo):
+    img_height, img_width, _ = image.shape
+    key_marks = sample.get_key_marks()[:, :2]
+    vector_eye = (key_marks[3] - key_marks[0])
+    degrees = mo.get_angle(vector_eye, np.array([100, 0]))
+    rotation_mat = cv2.getRotationMatrix2D(
+        ((img_width-1)/2.0, (img_height-1)/2.0), -degrees, 1)
+    image_rotated = cv2.warpAffine(
+        image, rotation_mat, (img_width, img_height))
+
+    return image_rotated, degrees
+
+
+def crop_face(image, marks):
+    img_height, img_width, _ = image.shape
+    x_min, y_min, _ = np.amin(marks, 0)
+    x_max, y_max, _ = np.amax(marks, 0)
+    scale = 1.5
+    side_length = max((x_max - x_min, y_max - y_min)) * scale
+    x_start = int(img_width / 2 - side_length / 2)
+    y_start = int(img_height / 2 - side_length / 2)
+    x_end = int(img_width / 2 + side_length / 2)
+    y_end = int(img_height / 2 + side_length / 2)
+
+    # In case the new bbox is out of image bounding.
+    border_width = 0
+    border_x = min(x_start, y_start)
+    border_y = max(x_end - img_width, y_end - img_height)
+    if border_x < 0 or border_y > 0:
+        border_width = max(abs(border_x), abs(border_y))
+        x_start += border_width
+        y_start += border_width
+        x_end += border_width
+        y_end += border_width
+        image_with_border = cv2.copyMakeBorder(image, border_width,
+                                               border_width,
+                                               border_width,
+                                               border_width,
+                                               cv2.BORDER_CONSTANT,
+                                               value=[0, 0, 0])
+        image_cropped = image_with_border[y_start:y_end,
+                                          x_start:x_end]
+    else:
+        image_cropped = image[y_start:y_end, x_start:x_end]
+
+    return image_cropped, border_width, (x_start, y_start, x_end, y_end)
+
+
+def show_debug_images(image_translated, marks_translated, image_rotated, marks_rotated, image_resized, mark_resized, padding, bbox):
+    # Resconstruct the padded image.
+    if padding != 0:
+        image_padded = cv2.copyMakeBorder(image_rotated, padding,
+                                          padding,
+                                          padding,
+                                          padding,
+                                          cv2.BORDER_CONSTANT,
+                                          value=[0, 0, 0])
+        cv2.rectangle(image_padded, (bbox[0], bbox[1]),
+                      (bbox[2], bbox[3]), (255, 255, 255), 3)
+        cv2.imshow("Padded", image_padded)
+    else:
+        cv2.rectangle(image_rotated, (bbox[0], bbox[1]),
+                      (bbox[2], bbox[3]), (255, 255, 255), 3)
+
+    # Draw translated marks.
+    fmd.mark_dataset.util.draw_marks(image_translated, marks_translated, 1)
+
+    # Draw rotated marks.
+    fmd.mark_dataset.util.draw_marks(image_rotated, marks_rotated, 1)
+
+    # Draw resized marks.
+    fmd.mark_dataset.util.draw_marks(image_resized, mark_resized, 1)
+
+    # Show them all.
+    cv2.imshow("Translated", image_translated)
+    cv2.imshow("Rotated", image_rotated)
+    cv2.imshow("Face sample", image_resized)
+
+    return cv2.waitKey() == 27
 
 
 if __name__ == "__main__":
@@ -165,10 +238,8 @@ if __name__ == "__main__":
     # Construct the datasets.
 
     # 300W
-    ds_300w = fmd.ds300w.DS300W("300w")
-    ds_300w.populate_dataset(ds300w_dir)
-
-    process(ds_300w)
+    # ds_300w = fmd.ds300w.DS300W("300w")
+    # ds_300w.populate_dataset(ds300w_dir)
 
     # # 300VW
     # ds_300vw = fmd.ds300vw.DS300VW("300vw")
@@ -190,9 +261,11 @@ if __name__ == "__main__":
     # ds_lfpw = fmd.lfpw.LFPW("lfpw")
     # ds_lfpw.populate_dataset(lfpw_dir)
 
-    # # WFLW
-    # ds_wflw = fmd.wflw.WFLW("wflw")
-    # ds_wflw.populate_dataset(wflw_dir)
+    # WFLW
+    ds_wflw = fmd.wflw.WFLW("wflw")
+    ds_wflw.populate_dataset(wflw_dir)
+
+    process(ds_wflw, 132)
 
     # # AFLW2000-3D
     # ds_aflw2k3d = fmd.AFLW2000_3D("AFLW2000_3D")
