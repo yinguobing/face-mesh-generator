e31b6ba2a074da60b4749a7d4892fa05248fa6d6
yinguobing <yinguobing@gmail.com>
2020-08-30 17:25:47 +0800

    Add script for viewing heatmap dataset

---
+++ view_heatmap_dataset.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
+from heatmap_record_operator import HeatmapRecordOperator
+from fmd.mark_dataset.util import draw_marks
+
+if __name__ == "__main__":
+    dataset = HeatmapRecordOperator(
+        "/home/robin/data/facial-marks/wflw/tfrecord/wflw_train.record")
+
+    for sample in dataset.parse_dataset():
+
+        image_decoded = tf.image.decode_image(sample['image/encoded']).numpy()
+        height = sample['image/height'].numpy()
+        width = sample['image/width'].numpy()
+        depth = sample['image/depth'].numpy()
+        filename = sample['image/filename'].numpy()
+        marks = tf.io.parse_tensor(sample['label/marks'], tf.double).numpy()
+        n_marks = sample['label/n_marks'].numpy()
+        heatmaps = tf.io.parse_tensor(
+            sample['heatmap/map'], tf.double).numpy()
+
+        print(filename, width, height, depth, heatmaps.shape)
+
+        # Use OpenCV to preview the image.
+        image = np.array(image_decoded, np.uint8)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Draw the landmark on image
+        draw_marks(image, marks*width, 1)
+        heatmaps = np.sum(heatmaps, axis=0) * 0.1
+
+        # Show the result
+        cv2.imshow("image", cv2.resize(image, (512, 512)))
+        cv2.imshow("Heatmap", cv2.resize(heatmaps, (512, 512), interpolation=cv2.INTER_AREA))
+        if cv2.waitKey() == 27:
+            break
